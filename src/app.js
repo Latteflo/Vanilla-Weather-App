5b441220a3a91ef05fac22d8aa5d34513ab9d6a6
Latteflo <sf.simion.f@gmail.com>
2022-01-25 16:39:24 +0100

    API integration

--- src/app.js
+++ src/app.js
@@ -22,35 +22,51 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#weather-forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
-      <div class="col-2"
-      <div class="weather-forecast-date"> ${day} </div>
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
-          src="https://ssl.gstatic.com/onebox/weather/48/rain_light.png"
-          alt="rain icon"
-          class="rain-icon"
-        >
-        <div class="weather-forecast-temperatures>
-        <span class=".weather-forecast-temperature-max">14°</span>
-        <br/>
-        <span class=".weather-forecast-temperature-min">11°</span>
-      </div>
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
       </div>
   `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
 }
+
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "2dc173b0ff579a19d5b495197fda8765";
